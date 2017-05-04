2f49a7f3ec7a7537084ef5f96691072251631506
Ana <a_caroline96@hotmail.com>
2017-05-03 22:01:33 -0300

    now we can print the output

--- parsec/Program.hs
+++ parsec/Program.hs
@@ -3,6 +3,7 @@ module Program where
 import Control.Monad
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Expr
+import Text.Show.Functions
 import Lexer
 
 -- Program (subprograms, main)
@@ -11,15 +12,24 @@ data Program = Program [Subprogram] Subprogram
 -- Generalization
 -- Function (parameters, return type, body)
 -- Procedure (parameters, body)
-data Subprogram = Function [Var] Type [Statement] | Procedure [Var] [Statement]
+data Subprogram = Function [Var] Type [Statement] | Procedure [Var] [Statement] 
+instance Show Subprogram where
+	show(Function a b c) = show a ++ " " ++ show b ++ " " ++ show c 
+	show(Procedure a b) = show a ++ " " ++ show b
 
-data Statement = Statement String -- TODO
+data Statement = Statement String  -- TODO
+instance Show Statement where
+	show(Statement a ) = show a
 
 -- Var (name, type)
 data Var = Var String String -- TODO
+instance Show Var where
+	show(Var a b ) = show a ++ " " ++ show b
 
 -- Type (name)
-data Type = Type String
+data Type = Type String 
+instance Show Type where
+	show(Type a ) = show a
 
 parseSubprograms :: Parser [Subprogram]
 parseSubprograms = many parseSubprogram
--- parsec/main.hs
+++ parsec/main.hs
@@ -2,5 +2,3 @@ import Text.ParserCombinators.Parsec
 import Program
 
 parseOWLS input = parse parseSubprogram "" input
-
-test = parseOWLS "main () {} oi"
