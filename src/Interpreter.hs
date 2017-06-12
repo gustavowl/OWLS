9c776b2439ae96d836cffec4c0589a0577202a04
Koruja <gustavoalvesbezerra@gmail.com>
2017-06-12 10:29:46 +0000

    Cleans output and deletes debugging commentaries

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -244,60 +244,20 @@ runStatement (Assignment (AssignEl array index) assign) state1 = do
 	return (state1, Continue) -- TODO
 
 runStatement (Assignment (AssignField struct field) assign) state1 = do
-	{-print "STRUCT"
-	print struct
-	print field
-	print assign -}
-	print "STATE BEFORE ASSIGNMENT"
-	print state1
 	let structVarName = getStructName (AssignField struct field)
 	scopeID <- getScopeID structVarName state1
-	--print "SCOPE"
-	--let var1 = getVar struct state1
-	--print var1
 	(t1, v1, state2) <- evalExpr assign state1
-	--print state2
 	(t2, v2) <- getVar (structVarName, scopeID) state2
 	if isUserType v2 then do
 		(t3, v3) <- searchFieldValue t2 (getVarValueList v2) field state2
-		{-
-		print "OIE"
-		print t1
-		print v1
-		print t2
-		print v2
-		print t3
-		print v3
-		print "XAU" -}
 		convertType t3 t1 
-		--print structVarName
-		--print scopeID
-		--print "WATCH?"
 		(AtomicType typename, _) <- getVar (structVarName, scopeID) state2
-		--print (AtomicType b, c)
-		--print "GOT VAR"
 		let types = getListUserTypes state2
-		--print types
-		--print "GOT USER TYPES LIST"
 		let (_, decs) = getUserType typename types
-		--print decs
-		--print "GOT USERTYPE"
-
 		let newStruct = updateStruct v1 v2 (AssignVar field) decs
-		{-
-		print newStruct
-		print "THIS IS WHAT SHOULD BE PRINTED"
-		let state42 = (updateVar (UserValue newStruct) (structVarName, scopeID) state2)
-		print state42
-		print "THIS WAS STATE42" -}
-		let state3 = (updateVar (UserValue newStruct) (structVarName, scopeID) state2)
-		print "STATE AFTER ASSIGNMENT"
-		print state3
-		return (state3, Continue)
-		--return ((updateVar (UserValue newStruct) (structVarName, scopeID) state2), Continue)
+		return ((updateVar (UserValue newStruct) (structVarName, scopeID) state2), Continue)
 	else
 		fail "Struct field not found."
-	--return (state1, Continue)
 
 runStatement (Assignment (AssignContent ptr) assign) state1 = do 
 	return (state1, Continue) -- TODO
